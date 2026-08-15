21d317d759fcc1c3488b43a4e3c843c73ad94249
christopheradaggett-creator <christopher.a.daggett@gmail.com>
2026-08-15 13:31:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,18 +1,22 @@
-const CACHE = 'our-kitchen-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'our-kitchen-b12';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(()=>{})));
-  self.skipWaiting();
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
 
+// Always go to the network. Only fall back to cache if the network is unreachable.
 self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
   e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+    fetch(e.request, { cache: 'no-store' })
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
